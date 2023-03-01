2f9824e5679535b8713d165724a1c5d87fa582ec
arfaoui-khaled <arfaoui.kld@gmail.com>
2023-03-01 19:05:09 +0100

    Use devise style devise views with devise-bootstrap-views, style layouts application with twitter-bootstrap-rails, use stock_quote

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, :initialize_stock_quote
+
+  def initialize_stock_quote
+    StockQuote::Stock.new(api_key: "sk_a61ec7dda17f47679260ec0fc
+294214b")
+  end
 end
