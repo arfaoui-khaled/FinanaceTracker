2bc77f1780f14ce41845130d4a08b91a676477c8
arfaoui-khaled <arfaoui.kld@gmail.com>
2023-03-02 19:29:26 +0100

    errors display and search action reformatting

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,21 +1,14 @@
 class StocksController < ApplicationController
 
   def search
-    if params[:stock].present?
+    if params[:stock].blank?
+      flash.now[:danger] = "You have entered an empty search string"
+    else
       @stock = Stock.new_from_lookup(params[:stock].to_s)
       #render json: @stock
-      if @stock
-        render partial: 'users/result'
-      else
-        flash[:danger] = "you have entered an incorrect symbol"
-        redirect_to my_portfolio_path
-      end
-    else
-      flash[:danger] = "You have entered an empty search string"
-      redirect_to my_portfolio_path
+      flash.now[:danger] = "you have entered an incorrect symbol" unless @stock
     end
-
+    render partial: 'users/result'
   end
-
 
 end
