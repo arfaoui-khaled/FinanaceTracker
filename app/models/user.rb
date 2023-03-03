7d47a0ddae4818c0747426d65af29143e6e3e618
arfaoui-khaled <arfaoui.kld@gmail.com>
2023-03-03 20:30:49 +0100

    update views and navigation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def full_name
-    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    return ("#{first_name} #{last_name}").strip if (first_name || last_name)
     "Anonymous"
   end
 
